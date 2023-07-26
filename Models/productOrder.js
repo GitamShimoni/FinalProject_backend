ed573a297905991e503d34092be37e99f9120f83
Gitam <gitam.shimoni@gmail.com>
2023-07-26 11:30:29 +0300

    changing-schemas

--- Models/productOrder.js
+++ Models/productOrder.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 
 const productOrderSchema = new mongoose.Schema({
+  ordersId: { type: String },
   productName: { type: String },
   dateOfOrder: { type: Date },
   quantity: { type: Number },
