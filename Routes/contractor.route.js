c890d328884036aa5d226a1a743a05e368c89877
SpeedyQ1 <sahar.y.ash@gmail.com>
2023-08-03 16:34:36 +0300

    contractor product controller

--- Routes/contractor.route.js
+++ Routes/contractor.route.js
@@ -3,12 +3,13 @@ const express = require("express");
 const router = express.Router();
 
 router.route("/create").post(contractorService.createContractor);
-router.route("/delete").delete(contractorService.deleteContractor);
+router.route("/delete").post(contractorService.deleteContractor);
 router.route("/editService").patch(contractorService.editContractorService);
 router.route("/add").post(contractorService.addServiceToContractor);
 router.route("/deleteService").post(contractorService.deleteContractorService);
 router.route("/getAllContractor").post(contractorService.getAllContractors)
 router.route("/getAllServices").post(contractorService.getAllServicesByContractorId)
+router.route("/addServicesArr").post(contractorService.addServiceArrToContractor)
   
 
 module.exports = router; 
