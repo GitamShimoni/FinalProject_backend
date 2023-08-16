a8782684b31458394c60af4788eb34f3f8206247
pazkopo01~ <pazkopo@gmail.com>
2023-08-16 20:51:06 +0300

    user-fixing-back

--- Routes/userRoute.js
+++ Routes/userRoute.js
@@ -1,12 +1,13 @@
 const express = require('express')
 const router = express.Router();
 const userController = require('../Controllers/userController')
+const middleware = require('../Middleware/middleware')
 
 router.post('/signup', userController.signup)
 router.post('/login', userController.login)
 router.get('/getUsers', userController.getAllUsers)
-router.post('/updateUsers', userController.updateUser)
-router.post('/deleteUser', userController.deleteUser)
+router.post('/updateUsers',  userController.updateUser)
+router.post('/deleteUser',  userController.deleteUser)
 router.post('/isToken', userController.isToken)
 
 
